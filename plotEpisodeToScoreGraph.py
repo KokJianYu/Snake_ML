b4506d89a47c5cbebe2d11354b77ef645e6666e1
Kok Jian Yu <kokjianyu@gmail.com>
2019-01-19 15:18:02 +0800

    Update plotting function to show best fit polynomial curve to points

--- plotEpisodeToScoreGraph.py
+++ plotEpisodeToScoreGraph.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import pandas as pd
 
 SIZE_INPUT_LAYER = 14
+#Model is hardcoded. Change to be identical to model in DeepQNetwork.py
 model = keras.Sequential()
 model.add(keras.layers.Dense(30, activation="relu", input_dim=SIZE_INPUT_LAYER))
 model.add(keras.layers.Dropout(0.15))
@@ -15,24 +16,24 @@ model.add(keras.layers.Dropout(0.15))
 model.add(keras.layers.Dense(30, activation="relu"))
 model.add(keras.layers.Dropout(0.15))
 model.add(keras.layers.Dense(3, activation="softmax"))
-opt = keras.optimizers.Adam(0.0005)
+opt = keras.optimizers.Adam(0.01)
 model.compile(loss="mse", optimizer=opt)
 
 PATH = f"model/"
 GAMES_PER_EPISODE = 10
+SHOW_GUI = False
 files = os.listdir(PATH)
 score_plot = []
 episode_plot = []
 for i in range(len(files)):
     weightsToUse = f"{PATH}{files[i]}"
     model.load_weights(weightsToUse)
-    #print(f"Current snake is from {files[i]}")
     totalScore = 0
     for j in range(GAMES_PER_EPISODE):
         score = 0
         counter = 0
         snakeML.newGameML()
-        snakeML.startGameML(showGui=False)
+        snakeML.startGameML(showGui=SHOW_GUI)
         while not snakeML.gameEnded and counter < 200: 
             state = np.reshape(snakeML.getInputLayer(), (1, SIZE_INPUT_LAYER))
             output = model.predict(state)
@@ -52,11 +53,16 @@ for i in range(len(files)):
 def plot_seaborn(array_counter, array_score):
     episodes = np.array(array_counter)
     scores = np.array(array_score)
-    data = {"episodes": episodes, "scores": scores}
-    df = pd.DataFrame(data)
-    sns.set(color_codes=True)
-    sns.lineplot(x="episodes", y="scores", data=df)
+    z = np.polyfit(episodes, scores, 3)
+    p = np.poly1d(z)
+    scores_new = p(episodes)
+    plt.plot(episodes, scores, 'o', episodes, scores_new)
     plt.show()
+    # data = {"episodes": episodes, "scores": scores}
+    # df = pd.DataFrame(data)
+    # sns.set(color_codes=True)
+    # sns.lineplot(x="episodes", y="scores", data=df)
+    # plt.show()
 
 
 plot_seaborn(episode_plot, score_plot)
